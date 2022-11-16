a8527424fd1ba9ecb9d04c1f9c8cf35cacd197c0
lorenzo-mignola <lo.mignola@gmail.com>
2022-11-16 11:26:53 +0100

    refactor(server): :recycle: use increment from prisma

--- apps/server/src/routes/posts/addLike.ts
+++ apps/server/src/routes/posts/addLike.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import prismaClient from "../../prismaClient";
 import { procedure } from "../../servers/trpcServer";
 
-const addLike = procedure.input(z.number()).mutation(async ({ input: postId }) => {
-  const post = await prismaClient.post.findUnique({ where: { id: postId } });
-  const newLike = (post?.like || 0) + 1;
-  return prismaClient.post.update({ where: { id: postId }, data: { like: newLike } });
-});
+const addLike = procedure
+  .input(z.number())
+  .mutation(async ({ input: postId }) =>
+    prismaClient.post.update({ where: { id: postId }, data: { like: { increment: 1 } } })
+  );
 
 export default addLike;
