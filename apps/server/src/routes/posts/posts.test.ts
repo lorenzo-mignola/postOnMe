021c5b908902f47619be5177cae0d6a1c1bd7b54
lorenzo-mignola <lo.mignola@gmail.com>
2022-11-16 17:14:33 +0100

    feat(server): :sparkles: add post pagination

--- apps/server/src/routes/posts/posts.test.ts
+++ apps/server/src/routes/posts/posts.test.ts
@@ -1,4 +1,4 @@
-import { beforeAll, describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import createContextInner from "../../../__test__/createContextInner";
 import createRouter from "../../appRouter";
 import prismaClient from "../../prismaClient";
@@ -12,19 +12,34 @@ const post = {
 };
 
 describe("posts", () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     const userCreated = await prismaClient.user.create({ data: user });
     const userId = userCreated.id;
-    await prismaClient.post.create({ data: { ...post, userId } });
+    // eslint-disable-next-line no-plusplus
+    for (let i = 1; i < 20; i++) {
+      // eslint-disable-next-line no-await-in-loop
+      await prismaClient.post.create({ data: { ...post, userId } });
+    }
+    await prismaClient.post.create({ data: { text: "21 post", userId, createdAt: new Date(1) } });
   });
 
-  test("should get a list with 1 post", async () => {
+  test("should get a list with some post post", async () => {
     const ctx = await createContextInner();
     const caller = createRouter.createCaller(ctx);
 
     const posts = await caller.posts();
 
-    expect(posts).toHaveLength(1);
+    expect(posts).toHaveProperty("length");
+    expect(posts).toHaveLength(20);
     expect(posts[0].author.name).toBe(user.name);
+  });
+
+  test("should get pagination", async () => {
+    const ctx = await createContextInner();
+    const caller = createRouter.createCaller(ctx);
+
+    const posts = await caller.posts({ page: 2 });
+
+    expect(posts[0].text).toBe("21 post");
   });
 });
--- apps/server/src/routes/posts/posts.ts
+++ apps/server/src/routes/posts/posts.ts
@@ -1,12 +1,32 @@
+import { z } from "zod";
 import prismaClient from "../../prismaClient";
 import { procedure } from "../../servers/trpcServer";
 
-const posts = procedure.query(async () =>
-  prismaClient.post.findMany({
-    include: {
-      author: true,
-    },
-  })
-);
+const posts = procedure
+  .input(
+    z
+      .object({
+        page: z.number().default(1),
+      })
+      .optional()
+  )
+  .query(async ({ input }) => {
+    const page = (input?.page || 1) - 1;
+    const skip = Math.max(page * 20 - 1, 0);
+
+    return prismaClient.post.findMany({
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+      ],
+      include: {
+        author: true,
+        comment: true,
+      },
+      take: 20,
+      skip,
+    });
+  });
 
 export default posts;
