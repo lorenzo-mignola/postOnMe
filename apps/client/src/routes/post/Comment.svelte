c8cc67f57416978ee59f8286cb87a6b12eba5ec7
lorenzo-mignola <lo.mignola@gmail.com>
2022-11-17 16:08:08 +0100

    test(client): :white_check_mark: test comment render

--- apps/client/src/routes/post/Comment.svelte
+++ apps/client/src/routes/post/Comment.svelte
@@ -7,14 +7,14 @@ export let comment: Post["comment"][number];
 
 <div class="mt-1" data-testid="comment-element">
   <hr class="border-gray-500" />
-  <p class="mt-2">
+  <p class="mt-2" data-testid="comment-comment">
     {comment.comment}
   </p>
   <span class="flex justify-between">
-    <p class="text-sm text-gray-400">
+    <p class="text-sm text-gray-400" data-testid="comment-author">
       by @{comment.author.name}
     </p>
-    <p class="text-sm text-gray-400">
+    <p class="text-sm text-gray-400" data-testid="comment-date">
       {formatDate(comment.commentedAt)}
     </p>
   </span>
